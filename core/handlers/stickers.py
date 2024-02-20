c043c7740468426f0b444f107dc014aca352e81f
akchonya <akchonyahokagivna@gmail.com>
2024-02-21 00:58:58 +0200

    update commands

--- core/handlers/stickers.py
+++ core/handlers/stickers.py
@@ -11,13 +11,6 @@ from aiogram.filters import Command
 router = Router()
 
 
-@router.message(Command("bunt"))
-async def bunt_sticker(message: types.Message):
-    await message.answer_sticker(
-        sticker="CAACAgIAAxkBAAEYRLBjKfNf9QtXKpeiBrBcSp5BNP2rwAACKiEAAgnVUUlHhGyMxOT0wykE"
-    )
-
-
 @router.message(Command("rusoriz"))
 async def rusorizt_sticker(message: types.Message):
     stickers = (
--- core/utils/commands.py
+++ core/utils/commands.py
@@ -4,13 +4,13 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 
 user_commands = [
     BotCommand(command="faq", description="корисна стаття"),
-    BotCommand(command="vahta", description="графік вахтерів"),
     BotCommand(command="weather_now", description="погода зараз"),
     BotCommand(command="weather_today", description="прогноз на сьогодні"),
     BotCommand(command="rekvizyty", description="реквізити на оплату за гурт"),
     BotCommand(command="laundry", description="сайт з пралками"),
     BotCommand(command="gumoreska", description="обрані гуморески"),
     BotCommand(command="svyato", description="свята сьогодні"),
+    BotCommand(command="rusoriz", description="ру-со-різ"),
 ]
 
 
